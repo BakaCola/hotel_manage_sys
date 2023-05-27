1fb49b1af2b1229bc61b9a8cc9512b9c1fb156c2
Cola <wenmingxiaoboke@outlook.com>
2023-05-27 17:12:52 +0800

    实现房间、房型信息的显示

--- hotel/views/room.py
+++ hotel/views/room.py
@@ -2,14 +2,31 @@ from django.core import serializers
 from django.shortcuts import render, redirect
 from django.views import View
 
-from hotel.models import Room
+from hotel.models import Room, RoomType
+
 
 
 class RoomList(View):
 	def get(self, request):
-		return render(request, "room_list.html")
+		room_data = Room.objects.all()
+		room_type_data = RoomType.objects.all()
+		context = {
+			"room_data": room_data,
+			"room_type_data": room_type_data,
+		}
+		return render(request, "room_list.html", context)
 
 
-class RoomManageList(View):
-	def get(self, request):
-		return render(request, "room_manage.html")
+class RoomEdit(View):
+	def get(request):
+		return render(request, "info_edit.html")
+
+
+class RoomAdd(View):
+	def get(request):
+		return render(request, "info_edit.html")
+
+
+class RoomDel(View):
+	def get(request):
+		return redirect("room_list")
