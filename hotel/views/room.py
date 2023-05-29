3d639f76c5124929b5f890be177e3d5b44b8098a
Cola <wenmingxiaoboke@outlook.com>
2023-05-30 00:29:09 +0800

    实现添加房间、房型功能

--- hotel/views/room.py
+++ hotel/views/room.py
@@ -2,8 +2,8 @@ from django.core import serializers
 from django.shortcuts import render, redirect
 from django.views import View
 
+from hotel.forms.forms import RoomModelForm, RoomTypeModelForm
 from hotel.models import Room, RoomType
-
 
 
 class RoomList(View):
@@ -18,15 +18,68 @@ class RoomList(View):
 
 
 class RoomEdit(View):
-	def get(request):
-		return render(request, "info_edit.html")
+	context = {
+		"title": "修改账户信息",
+		"addmoreCtr": 0,
+		"prv_info": "",
+	}
+
+	def get(self, request, pk):
+		room_obj = Room.objects.filter(pk=pk).first()
+		self.context["form"] = RoomModelForm(instance=room_obj)
+		return render(request, "info_edit.html", self.context)
 
 
 class RoomAdd(View):
-	def get(request):
-		return render(request, "info_edit.html")
+	context = {
+		"title": "添加客房信息",
+		"addMoreCtr": 1,
+		"prv_info": "",
+	}
+
+	def get(self, request):
+		self.context["form"] = RoomModelForm()
+		return render(request, "info_edit.html", self.context)
+
+	def post(self, request):
+		# print(request.POST)
+		form = RoomModelForm(request.POST)
+		if form.is_valid():
+			form.save()
+			if request.POST.get("addMore") == "1":
+				self.context["form"] = RoomModelForm()
+				self.context["prv_info"] = "客房 " + request.POST.get("room_number") + " 已添加"
+				return render(request, "info_edit.html", self.context)
+			return redirect("room_list")
+		self.context["form"] = form
+		return render(request, "info_edit.html", self.context)
 
 
 class RoomDel(View):
-	def get(request):
+	def get(self, request):
 		return redirect("room_list")
+
+
+class RoomTypeAdd(View):
+	context = {
+		"title": "添加房型信息",
+		"addMoreCtr": 1,
+		"prv_info": "",
+	}
+
+	def get(self, request):
+		self.context["form"] = RoomTypeModelForm()
+		return render(request, "info_edit.html", self.context)
+
+	def post(self, request):
+		# print(request.POST)
+		form = RoomTypeModelForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save()
+			if request.POST.get("addMore") == "1":
+				self.context["form"] = RoomTypeModelForm()
+				self.context["prv_info"] = "房型 " + request.POST.get("roomType_name") + " 已添加"
+				return render(request, "info_edit.html", self.context)
+			return redirect("room_list")
+		self.context["form"] = form
+		return render(request, "info_edit.html", self.context)
