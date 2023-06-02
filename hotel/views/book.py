fa2aef4be5566e74e72c74a063b68eca71cbbd6d
Cola <wenmingxiaoboke@outlook.com>
2023-06-02 10:17:46 +0800

    feat(预定房间): 新增查看房型功能

--- hotel/views/book.py
+++ hotel/views/book.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render, redirect
+from django.utils import timezone
 from django.views import View
 
-from hotel.models import Room
+from hotel.models import Room, RoomType
 
 
 class BookList(View):
+	context = {
+		"room_type": RoomType.objects.all(),
+		"today": timezone.now().date(),
+	}
+
 	def get(self, request):
-		room_data = Room.objects.all()
-		return render(request, "book_list.html", {"room_data": room_data})
+		return render(request, "book_list.html", self.context)
+		pass
 
 
+class BookCheck(View):
+	def get(self, request):
+		room_type = RoomType.objects.all()
+		return render(request, "book_check.html", {"room_type": room_type})
+		pass
+
+
+class BookDetail(View):
+	def get(self, request):
+		room_type = RoomType.objects.all()
+		return render(request, "book_detail.html", {"room_type": room_type})
+		pass
