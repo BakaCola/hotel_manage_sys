31c8ecd0c71a2e849e4ff73495aed7c34b632842
Cola <wenmingxiaoboke@outlook.com>
2023-06-03 00:44:28 +0800

    feat(预定房间): 添加日期选择，查询，限制功能
    - 加入日期选择，入住日期以及退房日期
    - 查询时显示查询动画，查询提示信息
    - 查询后停止加载动画，显示查询成功，更新剩余房间列表

--- hotel/views/book.py
+++ hotel/views/book.py
@@ -1,19 +1,49 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.views import View
 
-from hotel.models import Room, RoomType
+from hotel.models import Room, RoomType, OrderDetail
+
+
+def get_available(start, end, room_type_id):
+	all_room = Room.objects.filter(room_type_id=room_type_id)
+	booked_room = OrderDetail.objects.filter(order__order_check_in__lte=end,
+	                                         order__order_check_out__gte=start).values_list('room', flat=True)
+	available_room = all_room.exclude(id__in=booked_room)
+	# available_room_numbers = available_room.values_list('room_number', flat=True)
+	return available_room
 
 
 class BookList(View):
-	context = {
-		"room_type": RoomType.objects.all(),
-		"today": timezone.now().date(),
-	}
 
 	def get(self, request):
-		return render(request, "book_list.html", self.context)
-		pass
+		roomType_data = RoomType.objects.all()
+		room_count = []
+		today = timezone.now().strftime("%Y-%m-%d")
+		tomorrow = (timezone.now() + timezone.timedelta(days=1)).strftime("%Y-%m-%d")
+		st = request.GET.get("st", today)
+		ed = request.GET.get("ed", today)
+		for room_type in roomType_data:
+			room_count.append(get_available(st, ed, room_type.id).count())
+		context = {
+			"roomType_data": zip(roomType_data, room_count),
+			"st": st,
+			"ed": ed,
+			"today": today,
+			"tomorrow": tomorrow
+		}
+		return render(request, "book_list.html", context)
+
+	def post(self, request):
+		st = request.POST.get("st")
+		ed = request.POST.get("ed")
+		roomType_data = RoomType.objects.all()
+		room_count = {}
+		for room_type in roomType_data:
+			room_count[room_type.id] = get_available(st, ed, room_type.id).count()
+		# print(room_count)
+		return JsonResponse({"room_availability": room_count})
 
 
 class BookCheck(View):
